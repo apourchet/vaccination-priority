90f8278eb73e6bdbbd5143eb4f97eeb3aee9cdcf
Antoine Pourchet <antoine.pourchet@gmail.com>
2015-09-27 14:21:55 -0400

    its niiiice

--- mainsim.m
+++ mainsim.m
@@ -8,7 +8,7 @@ i_e = 2;
 d_y = 4;
 d_e = 1;
 
-p_y = 10;
+p_y = 1;
 f_y = 0.001;
 c_y = 0.1;
 
@@ -16,14 +16,15 @@ p_e = 0.1;
 f_e = 0.1;
 c_e = 0.05;
 
-alpha = 0.5;
-T_y = K_y - V*alpha;
-T_e = K_e - V*(1-alpha);
-
 f = figure; hold on;
-[tt, y_i, y_d, y_c] = flu_sim(T_y, p_y, f_y, c_y, 4);
-plot(tt, y_i, 'r');
-plot(tt, y_d, 'black');
-plot(tt, y_c, 'g');
+for alpha=0:.2:1
+    T_y = K_y - V*alpha;
+    T_e = K_e - V*(1-alpha);
+
+    [tt, y_i, y_d, y_c] = flu_sim(T_y, p_y, f_y, c_y, 4);
+    plot(tt, y_i, 'r');
+end
+% plot(tt, y_d, 'black');
+% plot(tt, y_c, 'g');
 % saveas(f, './young_infec.png', 'png');
 
