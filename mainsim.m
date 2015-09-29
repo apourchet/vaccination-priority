ac99d940856db3752da0536545084c2134be91b2
Antoine Pourchet <antoine.pourchet@gmail.com>
2015-09-28 20:45:55 -0400

    Fixed negative carrying cap LOL

--- mainsim.m
+++ mainsim.m
@@ -1,6 +1,6 @@
 K = 6000;
 K_y = 3600;
-K_e = 360;
+K_e = 600;
 V = 400;
 
 i_y = 1;
@@ -12,8 +12,8 @@ p_y = 1;
 f_y = 0.001;
 c_y = 0.1;
 
-p_e = 0.1;
-f_e = 0.1;
+p_e = 0.5;
+f_e = 0.05;
 c_e = 0.05;
 
 f = figure; hold on;
@@ -21,10 +21,10 @@ for alpha=0:.2:1
     T_y = K_y - V*alpha;
     T_e = K_e - V*(1-alpha);
 
-    [tt, y_i, y_d, y_c] = flu_sim(T_y, p_y, f_y, c_y, 4);
+    [tt, y_i, y_d, y_c] = flu_sim(T_e, p_e, f_e, c_e);
     plot(tt, y_i, 'r');
+    plot(tt, y_d, 'black');
+    plot(tt, y_c, 'g');
 end
-% plot(tt, y_d, 'black');
-% plot(tt, y_c, 'g');
 % saveas(f, './young_infec.png', 'png');
 
